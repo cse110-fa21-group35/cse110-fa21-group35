89656a659ca0ef73c2362bebb0b9d028da303f13
Zhongyi <zhw039@ucsd.edu>
2021-11-14 17:29:45 -0800

    updated

---
+++ source/components/change.js
@@ -0,0 +1,197 @@
+      counter1 = 1;
+      counter2 = 1;
+      counter3 = 1;
+      counter4 = 1;
+      counter5 = 1;
+      counter6 = 1;
+      counter7 = 1;
+      counter8 = 1;
+      counter9 = 1;
+      counter10 = 1;
+      counter11 = 1;
+      counter12 = 1;
+      counter13 = 1;
+      counter14 = 1;
+
+      function changeColor1() {
+        counter1 += 1;
+        let but = document.getElementById("1");
+
+        if (counter1 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor2() {
+        counter2 += 1;
+        let but = document.getElementById("2");
+
+        if (counter2 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor3() {
+        counter3 += 1;
+        let but = document.getElementById("3");
+
+        if (counter3 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor4() {
+        counter4 += 1;
+        let but = document.getElementById("4");
+
+        if (counter4 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor5() {
+        counter5 += 1;
+        let but = document.getElementById("5");
+
+        if (counter5 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor6() {
+        counter6 += 1;
+        let but = document.getElementById("6");
+
+        if (counter6 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor7() {
+        counter7 += 1;
+        let but = document.getElementById("7");
+
+        if (counter7 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor8() {
+        counter8 += 1;
+        let but = document.getElementById("8");
+
+        if (counter8 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor9() {
+        counter9 += 1;
+        let but = document.getElementById("9");
+
+        if (counter9 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor10() {
+        counter10 += 1;
+        let but = document.getElementById("10");
+
+        if (counter10 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor11() {
+        counter11 += 1;
+        let but = document.getElementById("11");
+
+        if (counter11 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor12() {
+        counter12 += 1;
+        let but = document.getElementById("12");
+
+        if (counter12 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor13() {
+        counter13 += 1;
+        let but = document.getElementById("13");
+
+        if (counter13 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+      function changeColor14() {
+        counter14 += 1;
+        let but = document.getElementById("14");
+
+        if (counter14 % 2 == 0) {
+          but.style.backgroundColor = "#FBE598";
+          but.style.color = "#405262";
+        } else {
+          but.style.backgroundColor = "#405262";
+          but.style.color = "white";
+        }
+      }
+
+      function show() {
+        let sidebar = document.getElementById("mySidebar");
+        sidebar.style.width = "245px";
+        let mainpage = document.getElementById("main");
+        mainpage.style.marginLeft = "245px";
+      }
+
+      function closeNav() {
+        let sidebar = document.getElementById("mySidebar");
+        sidebar.style.width = "0";
+        let mainpage = document.getElementById("main");
+        mainpage.style.marginLeft = "0";
+      }
