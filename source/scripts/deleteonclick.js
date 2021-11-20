995292becaafdf87b3d8484e1b0eac9e6a77476c
Rudy Zhang <ruz009@ucsd.edu>
2021-11-19 18:10:11 -0800

    delete on click final

--- source/scripts/deleteonclick.js
+++ source/scripts/deleteonclick.js
@@ -1,8 +1,11 @@
 let element = document.querySelector(".dropdown.delete");
 let mybox = document.querySelector(".box");
 
+//code dive works, need to test later 
+// cannot delete other users recipes without permission
+
 element.addEventListener("click", function() {
-        mybox.id
+        deleteRecipe(mybox.id);
 });
 
 
