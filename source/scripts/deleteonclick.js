694cbc6a4e8f54a9bc98390e5cead0e1075020d4
Yenyun035 <57857379+Yenyun035@users.noreply.github.com>
2021-11-21 00:54:20 -0800

    Format

--- source/scripts/deleteonclick.js
+++ source/scripts/deleteonclick.js
@@ -1,12 +1,9 @@
 let element = document.querySelector(".dropdown.delete");
 let mybox = document.querySelector(".box");
 
-//code dive works, need to test later 
+//code dive works, need to test later
 // cannot delete other users recipes without permission
 
-element.addEventListener("click", function() {
-        deleteRecipe(mybox.id);
+element.addEventListener("click", function () {
+  deleteRecipe(mybox.id);
 });
-
-
-
