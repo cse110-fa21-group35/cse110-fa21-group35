b05e79f2ccd2cc5e9a94ea8336dd537ce49cdd63
TacoTuesdayX <loganramos00@gmail.com>
2021-11-29 13:10:21 -0800

    linter doesn't like jest, will address l8r

--- __tests__/fixtures/example.js
+++ __tests__/fixtures/example.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-use-before-define */
 function example(a) {
   return a;
 }
 module.exports = example;
+/* eslint-enable no-use-before-define */
--- __tests__/unit/example.test.js
+++ __tests__/unit/example.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-use-before-define */
 const example = require('../fixtures/example');
 
 test('Example Unit Test', () => {
   expect(example(1)).toBe(1);
 });
+/* eslint-enable no-use-before-define */
