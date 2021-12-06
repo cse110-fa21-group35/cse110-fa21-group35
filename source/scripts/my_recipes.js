2981d2ffd2fe393c04b3268882984840b0952d9b
Yenyun035 <57857379+Yenyun035@users.noreply.github.com>
2021-12-05 17:29:28 -0800

    Fix nutri-label for customized recipe

--- source/scripts/my_recipes.js
+++ source/scripts/my_recipes.js
@@ -360,11 +360,11 @@ function createLeftContent(data) {
   //nutrition label;
   const label = document.createElement('img');
   label.className = 'nutr-label';
-  let nutrURL = data['nutrition']['nutritionImage'];
-  if (nutrURL === undefined) {
+  let nutrURL = data['nutrition'];
+  if (nutrURL === undefined || nutrURL['nutritionImage'] === undefined) {
     label.src = '/source/images/no-nutr-label-avail.jpg';
   } else {
-    label.src = `${nutrURL}?apiKey=48efb642c0b24eb586a3ba1d81ee738e`;
+    label.src = `${nutrURL['nutritionImage']}?apiKey=48efb642c0b24eb586a3ba1d81ee738e`;
   }
 
   left.appendChild(img);
